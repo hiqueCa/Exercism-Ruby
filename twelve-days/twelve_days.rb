ee6f165225d61a667dac8b35ac1d4fd2ea6a5c69
Henrique Caltram <henrivai@hotmail.com>
2021-12-17 15:20:11 -0300

    Alters the reference hash of song parts to an array, as the last version of code implemented it as a hash with L1 keys as integers, giving it a awful smell of forced array. As well, changes the implementation of methods to accomodate this change from hash to array.

--- twelve-days/twelve_days.rb
+++ twelve-days/twelve_days.rb
@@ -1,43 +1,43 @@
 module TwelveDays
 
-  DAY_ORDINALITY_PARTS = {
-    1 => {
+  DAY_ORDINALITY_PARTS = 
+    [{
       "first" => "a Partridge in a Pear Tree."
       }, 
-    2 => {
+    {
       "second" => "two Turtle Doves,"
     },
-    3 => {
+    {
       "third" => "three French Hens,"
     }, 
-    4 => {
+    {
       "fourth" => "four Calling Birds,"
     },
-    5 => {
+    {
       "fifth" => "five Gold Rings,"
     }, 
-    6 => {
+    {
       "sixth" => "six Geese-a-Laying,"
     },
-    7 => {
+    {
       "seventh" => "seven Swans-a-Swimming,"
     }, 
-    8 => {
+    {
       "eighth" => "eight Maids-a-Milking,"
     },
-    9 => {
+    {
       "ninth" => "nine Ladies Dancing,"
     }, 
-    10 => {
+    {
       "tenth" => "ten Lords-a-Leaping,"
     },
-    11 => {
+    {
       "eleventh" => "eleven Pipers Piping,"
     },
-    12 => {
+    {
       "twelfth" => "twelve Drummers Drumming,"
-    }
-  }
+    }]
+  
   
   def self.song
     complete_song
@@ -52,7 +52,7 @@ module TwelveDays
   def self.accumulate_complete_song_verse(verse)
     song_parts = []
     1.upto(verse) do |i|
-      song_part = fetch_ordinal_and_song_part(i).values[0]
+      song_part = fetch_ordinal_and_song_part(i-1).values[0]
       song_parts.insert(0, song_part)
     end
     
@@ -68,7 +68,7 @@ module TwelveDays
   end
 
   def self.complete_song_verse(verse)
-    ordinal = fetch_ordinal_and_song_part(verse).keys[0]
+    ordinal = fetch_ordinal_and_song_part(verse-1).keys[0]
     song_verse = join_song_parts_in_verse(verse)
 
     "On the #{ordinal} day of Christmas my true love gave to me: #{song_verse}"
